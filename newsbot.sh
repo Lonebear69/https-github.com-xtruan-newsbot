0b2bc1710cc3f18575c09f6f3dd2eef67a89eee3
xtruan <github@xtruan.com>
2017-01-26 21:06:06 -0500

    adding history archive

--- newsbot.sh
+++ newsbot.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd /var/www
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-INPUT=feed_urls.csv
+INPUT=$DIR/feed_urls.csv
 OLDIFS=$IFS
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 
-liveIndexFile=html/index.html
-newIndexFile=html/new_index.html
-feedXslFile=html/css/feed.xsl
+liveIndexFile=$DIR/html/index.html
+newIndexFile=$DIR/html/new_index.html
+feedXslFile=$DIR/html/css/feed.xsl
 
 date=`date`
 echo "<!DOCTYPE html><html><head><title>Newsbot</title><link rel=\"stylesheet\" href=\"css/feed.css\"></head><body>" > $newIndexFile
@@ -19,11 +19,11 @@ echo "<div class=\"content\"><ul>" >> $newIndexFile
 while read aFile aName aUrl
 do
 	# debug text
-	aXmlFile="html/$aFile.xml"
-	aHtmlFile="html/$aFile.html"
-	aHtmlFileUrl="$aFile.html"
-	aNoImgHtmlFile="html/noimg_$aFile.html"
-	aNoImgHtmlFileUrl="noimg_$aFile.html"
+	aXmlFile="$DIR/html/$aFile.xml"
+	aHtmlFile="$DIR/html/$aFile.html"
+	aHtmlFileUrl="$DIR/$aFile.html"
+	aNoImgHtmlFile="$DIR/html/noimg_$aFile.html"
+	aNoImgHtmlFileUrl="$DIR/noimg_$aFile.html"
 	
 	echo "File : $aXmlFile"
 	echo "Name : $aName"
@@ -33,7 +33,7 @@ do
 	date=`date`
 	start=`date +%s`
 	# call full-text-rss with php-cgi to dump xml file
-	php-cgi -f full-text-rss/makefulltextfeed.php url="$aUrl" max=10 links=preserve > $aXmlFile
+	php-cgi -f $DIR/full-text-rss/makefulltextfeed.php url="$aUrl" max=10 links=preserve > $aXmlFile
 	end=`date +%s`
 	runtime=$((end-start))
 	sed -i '$ d' $aXmlFile
@@ -58,3 +58,8 @@ IFS=$OLDIFS
 echo "</ul></div>" >> $newIndexFile
 echo "</body></html>" >> $newIndexFile
 mv $newIndexFile $liveIndexFile
+
+#archive to history
+mkdir -p $DIR/history
+export GZIP=-9
+tar cvzf $DIR/history/$(date -d "today" +"%Y_%m_%d_%H_%M_%S").tar.gz $DIR/html/*.html
